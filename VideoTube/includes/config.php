8fdadda8572e7d6502509cc6f05cf48369d1b54b
ahmad-dawod23 <ahmadibrahimdawod@gmail.com>
2023-12-12 23:27:33 +0300

    fixes

--- VideoTube/includes/config.php
+++ VideoTube/includes/config.php
@@ -5,7 +5,7 @@ session_start();
 date_default_timezone_set("Europe/Prague");
 
 try {
-    $con = new PDO("mysql:dbname=VideoTube;host=172.19.128.1", "newuser", "yes");
+    $con = new PDO("mysql:dbname=VideoTube;host=172.26.240.1", "newuser", "yes");
     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 }
 catch (PDOException $e) {
